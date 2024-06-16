daa4ff27e7a612cf3ef1354da178b60f1fac3ec2
Arthur <arthursh@outlook.com.br>
2024-06-16 11:06:19 -0300

    v16

---
+++ customize.sh
@@ -0,0 +1,3 @@
+read -p "Diretório para salvar o arquivo do dash(qualquer um): " dire
+cp /usr/share/unity/icons/launcher_bfb.png $dire
+cp ./launcher_bfb.png /usr/share/unity/icons/launcher_bfb.png
