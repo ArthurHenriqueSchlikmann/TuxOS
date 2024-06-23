b750d018bf68a8dc6b1ed834761ec6906c007550
Arthur <arthursh@outlook.com.br>
2024-06-23 18:38:41 -0300

    tux

--- customize.sh
+++ customize.sh
@@ -1,3 +1,6 @@
 read -p "Diretório para salvar o arquivo do dash(qualquer um): " dire
 cp /usr/share/unity/icons/launcher_bfb.png $dire
 cp ./launcher_bfb.png /usr/share/unity/icons/launcher_bfb.png
+echo "Agora você podera customizar o sistema!!"
+sleep 5
+unity-tweak-tool
