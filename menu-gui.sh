b95e1bff329ac143e7084528a73000e9c71d79b4
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-09-04 20:38:48 -0300

    instalador-gui-v1

--- menu-gui.sh
+++ menu-gui.sh
@@ -1,2 +1,11 @@
-senha=`kdialog --password "Digite sua senha"`
-echo "$senha"
+#!/usr/bin/env bash
+senha=`kdialog --title "Instalador de programas do TuxOS" --password "Digite sua senha"`
+if [ $? -eq 1 ]; then
+    kdialog --sorry "Operação cancelada pelo usuário"
+    exit 1
+fi
+categoria=`kdialog --radiolist "Selecione a categoria" \
+1 "Desenvolvimento" off \
+2 "Jogos" off \
+3 "Utilidades" off \
+4 "Internet" off`
