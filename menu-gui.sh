392bd7890f6e925dd086b57b947cceb45e909729
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-09-04 21:10:29 -0300

    instalador-gui-v1-patch1

--- menu-gui.sh
+++ menu-gui.sh
@@ -4,8 +4,17 @@ if [ $? -eq 1 ]; then
     kdialog --sorry "Operação cancelada pelo usuário"
     exit 1
 fi
-categoria=`kdialog --radiolist "Selecione a categoria" \
+categoria=`kdialog --title "Instalador de programas do TuxOS" --radiolist "Selecione a categoria" \
 1 "Desenvolvimento" off \
 2 "Jogos" off \
 3 "Utilidades" off \
-4 "Internet" off`
+4 "Internet" off` 
+case $categoria in
+    1)
+    CHOISE=$(kdialog --title "Instaldor de programas do TuxOS" --checklist "Selecione o que deseja instalar" \
+        1 "Arduino IDE" off \
+        2 "Visual Studio Code" off \
+        3 "Apache Netbeans IDE" off)
+    ;;
+esac
+echo ""
